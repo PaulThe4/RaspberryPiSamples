937c4a0c53c8936bae82a3be68eb4de216afe46f
Paul <sonipriyapaul@gmail.com>
2024-04-04 20:32:04 +0100

    Update Testwith2.py

--- Testwith2.py
+++ Testwith2.py
@@ -1,3 +1,5 @@
+#!/usr/bin python
+
 import cv2
 import os
 
@@ -8,16 +10,16 @@ interval = 5
 cap1 = cv2.VideoCapture(0)
 cap1.set(3,640)   
 cap1.set(4,480)  
-cap2 = cv2.VideoCapture(2)
-cap2.set(3,640)   
-cap2.set(4,480)  
+#cap2 = cv2.VideoCapture(2)
+#cap2.set(3,640)   
+#cap2.set(4,480)  
 
 while True:
     ret1,frame1 = cap1.read()
     cv2.imshow('Live1',frame1)
     
-    ret2,frame2 = cap2.read()
-    cv2.imshow('Live2',frame2)
+ #   ret2,frame2 = cap2.read()
+  #  cv2.imshow('Live2',frame2)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -30,6 +32,6 @@ while True:
      #   break
         
 cap1.release()
-cap2.release()
+#cap2.release()
 
 cv2.destroyAllWindows()
